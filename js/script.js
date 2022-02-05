89316e1db0cfdc080790f40e2de95753a050f4a9
Gurb-S <OrganicMintz@gmail.com>
2022-02-05 07:55:28 +0100

    add basic random functionality

--- js/script.js
+++ js/script.js
@@ -14,6 +14,10 @@
 //   .then(data => items.push(data));
 
 // console.log(items);
+const item = document.getElementById('item');
+const btn = document.getElementById('btn');
+const item2 = document.getElementById('item2');
+const btn2 = document.getElementById('btn2');
 
 async function getJSON(){
     const response = await fetch('/battle.json');
@@ -21,12 +25,74 @@ async function getJSON(){
     console.log(data);
     const weaponsArray = data.weapons;
     console.log(weaponsArray)
-    console.log(weaponsArray[0].displayName);
+    // console.log(weaponsArray[0].displayName);
     let weapons = [];
     weaponsArray.forEach(weapon => {
         weapons.push(weapon.displayName);
     });
-    console.log(weapons);
+
+    // console.log(weapons);
+    const helmetArray = data.helmet;
+    console.log(helmetArray);
+    console.log(helmetArray[0].displayName);
+    let helmets = [];
+    helmetArray.forEach(helmet => {
+        helmets.push(helmet.displayName);
+    })
+    console.log(helmets);
+
+    const chestPlateArray = data.chestplate;
+    console.log(chestPlateArray);
+    console.log(chestPlateArray[0].displayName);
+    let chestplates = [];
+    chestPlateArray.forEach(chestplate => {
+        chestplates.push(chestplate.displayName);
+    })
+    console.log(chestplates);
+
+    const leggingsArray = data.leggings;
+    console.log(leggingsArray);
+    console.log(leggingsArray[0].displayName);
+    let leggings = [];
+    leggingsArray.forEach(legging => {
+        leggings.push(legging.displayName);
+    })
+    console.log(leggings);
+
+    const bootsArray = data.boots;
+    console.log(bootsArray);
+    console.log(bootsArray[0].displayName);
+    let boots = [];
+    bootsArray.forEach(boot => {
+        boots.push(boot.displayName);
+    })
+    console.log(boots);
+
+    
+    getRandomItem(weapons);
+    getRandomItem(helmets);
+    getRandomItem(chestplates);
+    getRandomItem(leggings);
+    getRandomItem(boots);
+    
 }
 
-getJSON();
+function getRandomItem(arr){
+    
+    const randomNumber = Math.floor(Math.random() * arr.length);
+    let randomItem = arr[randomNumber];
+    console.log(randomItem);
+    item.innerHTML += `<h2>${randomItem}</h2>`;
+
+}
+//getJSON();
+btn.addEventListener('click', () => {
+    item.innerHTML = '';
+    getJSON();
+});
+
+btn2.addEventListener('click', () => {
+    item2.innerHTML = '';
+    getJSON();
+});
+
