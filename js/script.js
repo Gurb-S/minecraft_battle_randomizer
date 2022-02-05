a8a513fc55412478027d1405e801506b5be8976d
Gurb-S <OrganicMintz@gmail.com>
2022-02-05 07:12:47 +0100

    seperate weapons from file

--- js/script.js
+++ js/script.js
@@ -1,9 +1,32 @@
-function fetchData(url){
-    const response = fetch(url)
-        .then(response => response.json())
-    return response;
+// function getJSON(url){
+//     try{
+//       const response = fetch(url);
+//       return response.json();
+//     } catch (error) {
+//       throw error;
+//     }
+//   }
+
+// let items = [];
+// let allItems = items[0];
+
+// getJSON('/battle.json')
+//   .then(data => items.push(data));
+
+// console.log(items);
+
+async function getJSON(){
+    const response = await fetch('/battle.json');
+    const data = await response.json();
+    console.log(data);
+    const weaponsArray = data.weapons;
+    console.log(weaponsArray)
+    console.log(weaponsArray[0].displayName);
+    let weapons = [];
+    weaponsArray.forEach(weapon => {
+        weapons.push(weapon.displayName);
+    });
+    console.log(weapons);
 }
 
-const allItems = fetchData('/battle.json');
-console.log(allItems);
-console.log(allItems)
+getJSON();
